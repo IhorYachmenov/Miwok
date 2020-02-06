b4c69bf86bde4f61b19b8ab45e571fc428a483ff
IhorYachmenov <IlLoganlI@Hotmail.com>
2020-02-06 16:41:55 +0200

    added new class and activity

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,7 +3,9 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -31,11 +33,35 @@ public class NumbersActivity extends AppCompatActivity {
 
         Log.v("NumbersActivity", "Numbers at index: " + numbers.get(0));
 
-        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+        //LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
 
-        TextView displayWords = new TextView(this);
-        displayWords.setText(numbers.get(0));
-        rootView.addView(displayWords);
+        int counter = 0;
+
+
+//        TextView displayWords = new TextView(this);
+//        displayWords.setText(numbers.get(0));
+//        rootView.addView(displayWords);
+
+//        while (counter < numbers.size()) {
+//
+//            TextView displayWords = new TextView(this);
+//            displayWords.setText(numbers.get(counter));
+//            rootView.addView(displayWords);
+//            counter++;
+//        }
+
+//        for (int index = 0; index < numbers.size(); index++) {
+//            TextView displayWords = new TextView(this);
+//            displayWords.setText(numbers.get(index));
+//            rootView.addView(displayWords);
+//        }
+
+
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, numbers);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(itemsAdapter);
 
     }
 }
