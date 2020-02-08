703a48576e9eab4ade0aaff0ae811d4ae3b6faff
IhorYachmenov <IlLoganlI@Hotmail.com>
2020-02-09 00:58:05 +0200

    added AudioManager.OnAudioFocusChangeListener to number and color activities

--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.miwok;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
